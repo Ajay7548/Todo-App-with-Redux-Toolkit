b2fc89ded358a14e4a6ec7cbacd6c03c9e116ecc
Ajay7548 <mouryaajay7548>
2025-01-18 20:32:42 +0530

    TailwindCss changes pending

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -42,7 +42,7 @@ const AddTodo = () => {
       />
       <button
         type="submit"
-        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+        className="text-white bg-indigo-600 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
       >
         Add Todo
       </button>
